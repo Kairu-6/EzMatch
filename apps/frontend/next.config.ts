d45c49d68bb216198d421fb16b54469618b5ce1d
Hadi hilmi <uzumakirikateki@gmail.com>
2026-05-26 08:30:14 +0800

    chore : pushinggg

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* It goes right here at the root level instead of experimental! */
+  allowedDevOrigins: ["192.168.56.1", "localhost:3000"]
 };
 
 export default nextConfig;
